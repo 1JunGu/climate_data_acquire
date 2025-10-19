e5a4e5ab9e12f8c76065670ee2eb77b6c4ad8bcf
Jun Gu <kyuinkoo@gmail.com>
2025-10-20 00:43:38 +0800

    fix(ERA5): make download script complete and usable

--- ERA5/download_ERA5_drive_MPAS_multi_init.py
+++ ERA5/download_ERA5_drive_MPAS_multi_init.py
@@ -41,13 +41,12 @@ def retrieve_era5_data(dataset, variables, year, month, day, time, file_path, pl
     c.retrieve(name, params, file_path)
 
 # Main program
-#name of Tropical Cyclone
-dir_name='2305Doksuri'
+dir_name='21.7'
 #initial time of the forecast List
-init_time=['2023-07-27T19:00:00','2023-07-27T23:00:00']
+init_time_list=['2021-07-18T09:00:00','2021-07-18T10:00:00','2021-07-18T12:00:00', '2021-07-18T21:00:00', '2021-07-18T22:00:00']
 #period of surface
-start_date = '2023-07-26'
-end_date = '2023-08-03'
+start_date = '2021-07-18'
+end_date = '2021-07-21'
 
 #create a directory named dir_name
 if not os.path.exists(dir_name):
@@ -60,14 +59,14 @@ queue = multiprocessing.Queue()
 jobs = []
 
 #Download the data for init_final
-for init_hour in pd.date_range(start=init_time[0], end=init_time[1], freq='h'):
-    init_hour_str = init_hour.strftime("%Y-%m-%dT%H:%M:%S")
-    print(init_hour_str)
-    print(type(init_hour_str))
-    init_year = init_hour_str[:4]
-    init_month = init_hour_str[5:7]
-    init_day = init_hour_str[8:10]
-    init_hour = init_hour_str[11:16]
+#for init_hour in pd.date_range(start=init_time[0], end=init_time[1], freq='h'):
+for init_time in init_time_list:
+    #init_hour_str = init_hour.strftime("%Y-%m-%dT%H:%M:%S")
+
+    init_year = init_time[:4] #2021
+    init_month = init_time[5:7] #07
+    init_day = init_time[8:10] #18
+    init_hour = init_time[11:16] #09:00
 
     pl_var_list=['geopotential', 'relative_humidity', 'temperature','u_component_of_wind', 'v_component_of_wind']
     pl_list=[
@@ -105,58 +104,58 @@ for init_hour in pd.date_range(start=init_time[0], end=init_time[1], freq='h'):
     p.start()
 
 #Download the data for surface_update
-#sfc_var_list = ['land_sea_mask', 'sea_ice_cover', 'sea_surface_temperature']
-#
-#time_list = [
-#    '00:00', '01:00', '02:00', '03:00',
-#    '04:00', '05:00', '06:00', '07:00',
-#    '08:00', '09:00', '10:00', '11:00',
-#    '12:00', '13:00', '14:00', '15:00',
-#    '16:00', '17:00', '18:00', '19:00',
-#    '20:00', '21:00', '22:00', '23:00',
-#]
-#
-#start_year = start_date[:4]
-#start_month = start_date[5:7]
-#end_year = end_date[:4]
-#end_month = end_date[5:7]
-#
-#if start_year == end_year and start_month == end_month:
-#    # Retrieve data for a single month
-#    day_list = pd.date_range(start_date, end_date, freq='D').strftime('%d').tolist()
-#    file_path = os.path.join(dir_name, f'sfc_{start_date}-{end_date}.grib')
-#    #retrieve_era5_data(sfc_var_list, start_year, start_month, day_list, time_list, file_path)
-#    p = multiprocessing.Process(target=retrieve_era5_data, args=('sl', sfc_var_list, start_year, start_month, day_list, time_list, file_path))
-#    jobs.append(p)
-#    p.start()
-#else:
-#    # Retrieve data for multiple months
-#    ## Retrieve data for the first month
-#    last_day = calendar.monthrange(int(start_year), int(start_month))[1]
-#    day_list = pd.date_range(start_date, f'{start_year}-{start_month}-{last_day}', freq='D').strftime('%d').tolist()
-#    file_path = os.path.join(dir_name, f'sfc_{start_date}-{start_year}-{start_month}-{last_day}.grib')
-#    #retrieve_era5_data(sfc_var_list, start_year, start_month, day_list, time_list, file_path)
-#    p = multiprocessing.Process(target=retrieve_era5_data, args=('sl',sfc_var_list, start_year, start_month, day_list, time_list, file_path))
-#    jobs.append(p)
-#    p.start()
-#
-#    # Retrieve data for the months in between
-#    for year_month in pd.date_range(start=f'{start_year}-{int(start_month)+1:02}', end=f'{end_year}-{int(end_month)-1:02}', freq='MS'):
-#        year_month_str = year_month.strftime('%Y-%m')
-#        last_day = calendar.monthrange(int(year_month_str[:4]), int(year_month_str[5:7]))[1]
-#        day_list = pd.date_range(f'{year_month_str}-01', f'{year_month_str}-{last_day}', freq='D').strftime('%d').tolist()
-#        file_path = os.path.join(dir_name, f'sfc_{year_month_str}.grib')
-#        #retrieve_era5_data(sfc_var_list, year_month_str[:4], year_month_str[5:7], day_list, time_list, file_path)
-#        p = multiprocessing.Process(target=retrieve_era5_data, args=('sl',sfc_var_list, year_month_str[:4], year_month_str[5:7], day_list, time_list, file_path))
-#        jobs.append(p)
-#        p.start()
-#
-#    # Retrieve data for the last month
-#    day_list = pd.date_range(f'{end_year}-{end_month}-01', end_date, freq='D').strftime('%d').tolist()
-#    file_path = os.path.join(dir_name, f'sfc_{end_year}-{end_month}-01-{end_date}.grib')
-#    p = multiprocessing.Process(target=retrieve_era5_data, args=('sl',sfc_var_list, end_year, end_month, day_list, time_list, file_path))
-#    jobs.append(p)
-#    p.start()
+sfc_var_list = ['sea_ice_cover', 'sea_surface_temperature']
+
+time_list = [
+    '00:00', '01:00', '02:00', '03:00',
+    '04:00', '05:00', '06:00', '07:00',
+    '08:00', '09:00', '10:00', '11:00',
+    '12:00', '13:00', '14:00', '15:00',
+    '16:00', '17:00', '18:00', '19:00',
+    '20:00', '21:00', '22:00', '23:00',
+]
+
+start_year = start_date[:4]
+start_month = start_date[5:7]
+end_year = end_date[:4]
+end_month = end_date[5:7]
+
+if start_year == end_year and start_month == end_month:
+    # Retrieve data for a single month
+    day_list = pd.date_range(start_date, end_date, freq='D').strftime('%d').tolist()
+    file_path = os.path.join(dir_name, f'sfc_{start_date}-{end_date}.grib')
+    #retrieve_era5_data(sfc_var_list, start_year, start_month, day_list, time_list, file_path)
+    p = multiprocessing.Process(target=retrieve_era5_data, args=('sl', sfc_var_list, start_year, start_month, day_list, time_list, file_path))
+    jobs.append(p)
+    p.start()
+else:
+    # Retrieve data for multiple months
+    ## Retrieve data for the first month
+    last_day = calendar.monthrange(int(start_year), int(start_month))[1]
+    day_list = pd.date_range(start_date, f'{start_year}-{start_month}-{last_day}', freq='D').strftime('%d').tolist()
+    file_path = os.path.join(dir_name, f'sfc_{start_date}-{start_year}-{start_month}-{last_day}.grib')
+    #retrieve_era5_data(sfc_var_list, start_year, start_month, day_list, time_list, file_path)
+    p = multiprocessing.Process(target=retrieve_era5_data, args=('sl',sfc_var_list, start_year, start_month, day_list, time_list, file_path))
+    jobs.append(p)
+    p.start()
+
+    # Retrieve data for the months in between
+    for year_month in pd.date_range(start=f'{start_year}-{int(start_month)+1:02}', end=f'{end_year}-{int(end_month)-1:02}', freq='MS'):
+        year_month_str = year_month.strftime('%Y-%m')
+        last_day = calendar.monthrange(int(year_month_str[:4]), int(year_month_str[5:7]))[1]
+        day_list = pd.date_range(f'{year_month_str}-01', f'{year_month_str}-{last_day}', freq='D').strftime('%d').tolist()
+        file_path = os.path.join(dir_name, f'sfc_{year_month_str}.grib')
+        #retrieve_era5_data(sfc_var_list, year_month_str[:4], year_month_str[5:7], day_list, time_list, file_path)
+        p = multiprocessing.Process(target=retrieve_era5_data, args=('sl',sfc_var_list, year_month_str[:4], year_month_str[5:7], day_list, time_list, file_path))
+        jobs.append(p)
+        p.start()
+
+    # Retrieve data for the last month
+    day_list = pd.date_range(f'{end_year}-{end_month}-01', end_date, freq='D').strftime('%d').tolist()
+    file_path = os.path.join(dir_name, f'sfc_{end_year}-{end_month}-01-{end_date}.grib')
+    p = multiprocessing.Process(target=retrieve_era5_data, args=('sl',sfc_var_list, end_year, end_month, day_list, time_list, file_path))
+    jobs.append(p)
+    p.start()
 #wait for all processes to finish
 print("All Requests finished !")
 for p in jobs:
